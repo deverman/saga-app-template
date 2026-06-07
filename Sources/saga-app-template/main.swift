8a38aadb0bde749fcd4294a520f305a1f4d7ca40
Brent Deverman <brent@deverman.org>
2026-06-07 14:43:39 +0800

    Adjust needed packages, and build out home page and privacy page.

---
+++ Sources/saga-app-template/main.swift
@@ -0,0 +1,39 @@
+import Foundation
+import Saga
+import SagaParsleyMarkdownReader
+import SagaSwimRenderer
+
+enum SiteMetadata {
+  static let url = URL(string: "http://www.example.com")!
+  static let name = "Example App Landing Page"
+  static let author = "Brent Deverman"
+}
+
+struct ArticleMetadata: Metadata {
+  let tags: [String]
+  var summary: String?
+}
+
+try await Saga(input: "content", output: "deploy")
+  .register(
+    folder: "articles",
+    metadata: ArticleMetadata.self,
+    readers: [.parsleyMarkdownReader(
+        markdownOptions: [.hardBreaks, .smartQuotes, .markdownAttributes],
+        syntaxExtensions: [.autolink, .strikethrough, .table, .tasklist]
+)],
+    writers: [
+      .itemWriter(swim(renderArticle)),
+      .listWriter(swim(renderArticles)),
+      .tagWriter(swim(renderTag), tags: \.metadata.tags),
+    ]
+  )
+  .register(
+    metadata: EmptyMetadata.self,
+    readers: [.parsleyMarkdownReader(
+    markdownOptions: [.hardBreaks, .smartQuotes, .markdownAttributes],
+        syntaxExtensions: [.autolink, .strikethrough, .table, .tasklist]
+        )],
+    writers: [.itemWriter(swim(renderPage))]
+  )
+  .run()
